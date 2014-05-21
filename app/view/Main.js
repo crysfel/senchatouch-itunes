a9fd4d20f277b58fc9c8b24b9b3127a4eb94c797
Crysfel Villa <crysfel@bleext.com>
2014-05-21 18:37:40 -0400

    Adding the main layout

--- app/view/Main.js
+++ app/view/Main.js
@@ -1,50 +1,29 @@
+/**
+ * @class Itunes.view.Main
+ * @extends Ext.Container
+ * The main container for the entire application
+ */
+
 Ext.define('Itunes.view.Main', {
-    extend: 'Ext.tab.Panel',
-    xtype: 'main',
-    requires: [
-        'Ext.TitleBar',
-        'Ext.Video'
+    extend: 'Ext.Container',
+    xtype : 'main',
+    requires : [
+        
     ],
-    config: {
-        tabBarPosition: 'bottom',
-
-        items: [
-            {
-                title: 'Welcome',
-                iconCls: 'home',
-
-                styleHtmlContent: true,
-                scrollable: true,
-
-                items: {
-                    docked: 'top',
-                    xtype: 'titlebar',
-                    title: 'Welcome to Sencha Touch 2'
-                },
-
-                html: [
-                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
-                    "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
-                    "and refresh to change what's rendered here."
-                ].join("")
-            },
-            {
-                title: 'Get Started',
-                iconCls: 'action',
-
-                items: [
-                    {
-                        docked: 'top',
-                        xtype: 'titlebar',
-                        title: 'Getting Started'
-                    },
-                    {
-                        xtype: 'video',
-                        url: 'http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c',
-                        posterUrl: 'http://b.vimeocdn.com/ts/261/062/261062119_640.jpg'
-                    }
-                ]
-            }
-        ]
+    config : {
+        layout  : 'vbox',
+        items   : [{
+            //we are going to move this container to a class
+            xtype   : 'container',
+            height  : 120,
+            style   : 'background:#bdc3c7', //we will remove this later
+            html    : 'The Header'
+        },{
+            //we are going to move this container to a class
+            xtype   : 'component',
+            flex    : 1,
+            style   : 'background:#ecf0f1', //we will remove this later
+            html    : 'The Results'
+        }]
     }
 });
